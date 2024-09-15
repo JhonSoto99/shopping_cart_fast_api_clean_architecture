2b3e7e0c477f8a9fbcb376265ac831f500bbe1b9
Jhon Soto <jhon.soto@aflore.co>
2024-09-14 19:42:42 -0500

    Feat: agrega las entidades producto y evento a la capa de dominio.

---
+++ app/domain/enitities/item.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from uuid import UUID, uuid4
+
+
+@dataclass(kw_only=True)
+class ItemBase:
+    price: int
+    name: str
+    thumbnail: str
+    description: str
+    stock: int
+    id: UUID = field(default_factory=uuid4)
+
+@dataclass(kw_only=True)
+class Product(ItemBase):
+    weight: float
+    brand: str
+
+@dataclass(kw_only=True)
+class Event(ItemBase):
+    organizer: str
+    event_date: datetime
+    venue: str
